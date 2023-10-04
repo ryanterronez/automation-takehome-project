20d147f54be7e19df0716d6480ee1139526c275a
ryanterronez <ryanterronez@gmail.com>
2023-10-04 15:29:36 -0500

    updating github action to run test and upload csv artifact

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   expect: {
     timeout: 15 * 1000,
   },
+  timeout: 60 * 1000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -15,6 +16,6 @@ export default defineConfig({
     trace: 'on-first-retry',
     browserName: 'chromium',
     viewport: null,
-    headless: false,
+    headless: true,
   },
 });
--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -2,8 +2,6 @@ import { test } from '@playwright/test';
 import { ECommercePage } from '../pages/ecommerce-page';
 import Item from '../models/item.model';
 
-const searchTerms: string[] = ['laptop', 'tv', 'phone'];
-
 test('search item, get prices', async ({ page }) => {
     const ecommercePage = new ECommercePage(page);
     const searchTerm: string = 'laptop';
@@ -24,25 +22,3 @@ test('search item, get prices', async ({ page }) => {
 
     ecommercePage.writeToCsv(items, searchTerm);
 });
-
-for (const searchTerm of searchTerms) {
-  test.skip(`search ${searchTerm}, get prices`, async ({ page }) => {
-      const ecommercePage = new ECommercePage(page);
-      await ecommercePage.goto();
-      await ecommercePage.searchItem(searchTerm);
-      await ecommercePage.sortSearchResults();
-
-      const itemOne: Item = await ecommercePage.getItem(0, searchTerm);
-      await ecommercePage.page.goBack();
-
-      const itemTwo: Item = await ecommercePage.getItem(1, searchTerm);
-      await ecommercePage.page.goBack();
-
-      const itemThree: Item = await ecommercePage.getItem(2, searchTerm);
-      await ecommercePage.page.goBack();
-
-      const items: Item[] = [itemOne, itemTwo, itemThree];
-
-      ecommercePage.writeToCsv(items, searchTerm);
-  });
-}
